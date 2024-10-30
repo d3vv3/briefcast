3e1b2d5e296ba53e6bcd0ac221968b63f72211a0
d3vv3 <jaimecondel@gmail.com>
2024-10-31 00:17:25 +0100

    feat: use gemini for large context

--- trimmer/main.py
+++ trimmer/main.py
@@ -1,18 +1,14 @@
-from os import getenv
+import os
 
 from fastapi import FastAPI, UploadFile
-from langchain.chains import MapReduceDocumentsChain, ReduceDocumentsChain
-from langchain.chains.combine_documents.stuff import StuffDocumentsChain
-from langchain.chains.llm import LLMChain
-from langchain_core.prompts import PromptTemplate
-from langchain_openai import OpenAI
-from langchain_text_splitters import CharacterTextSplitter
+import google.generativeai as genai
 
 app = FastAPI()
-llm = OpenAI(temperature=0.3, model="gpt-3.5-turbo-instruct")
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
-prompt_chunk_summary = PromptTemplate.from_template(
-    """**Input**: A transcript file with timestamps (format: [start_time -> end_time] text)
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+prompt = """**Input**: A transcript file with timestamps (format: [start_time -> end_time] text)
 **Task**: Filter out lines that are not relevant to the main topic of the podcast transcription, condensing the transcript to focus on the primary discussion.
 **Objective**: Reduce the overall time of the transcription by removing:
     - Off-topic conversations
@@ -44,57 +40,6 @@ Input transcription:
 
 Output:
 """
-)
-
-prompt_summaries_reduce = PromptTemplate.from_template(
-    """**Input**: Chunks of a podcast transcript with timestamps (format: [start_time -> end_time] text)
-**Output**: A condensed transcript file with timestamps (format: [start_time -> end_time] text), containing only the relevant lines that align with the main topic of the podcast.
-
-**Note**: The output transcript should maintain the original timestamp format and only include the relevant lines that align with the main topic.
-**Important**: Only output the condensed transcript file, without any additional text or explanations. The output should consist solely of the relevant lines with timestamps, formatted as shown above.
-
-Input:
-
-{transcription}
-
-Output:
-"""
-)
-
-# The prompt is the same for both the map and reduce chains
-map_chain = LLMChain(llm=llm, prompt=prompt_chunk_summary)
-reduce_chain = LLMChain(llm=llm, prompt=prompt_summaries_reduce)
-
-combine_documents_chain = StuffDocumentsChain(
-    llm_chain=reduce_chain, document_variable_name="transcription"
-)
-
-# For each transcription chunk, highlight the relevant parts
-# (smaller transcription)
-reduce_documents_chain = ReduceDocumentsChain(
-    combine_documents_chain=combine_documents_chain,
-    token_max=3000,
-)
-
-# Combine the smaller transcriptions into a single document and reduce them
-map_reduce_chain = MapReduceDocumentsChain(
-    llm_chain=map_chain,
-    reduce_documents_chain=reduce_documents_chain,
-    document_variable_name="transcription",
-    return_intermediate_steps=True,
-)
-text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
-    separator="\n", chunk_size=2000, chunk_overlap=0
-)
-
-from langfuse.callback import CallbackHandler
-
-langfuse_handler = CallbackHandler(
-    getenv("LANGFUSE_PK"),
-    getenv("LANGFUSE_SK"),
-    getenv("LANGFUSE_URL"),
-)
-
 
 @app.get("/")
 def read_root():
@@ -104,9 +49,5 @@ def read_root():
 @app.post("/trim")
 def trim(transcription_file: UploadFile):
     transcription_text = transcription_file.file.read().decode("utf-8")
-    split_transcription = text_splitter.create_documents([transcription_text])
-    response = map_reduce_chain.invoke(
-        split_transcription,
-        config={"callbacks": [langfuse_handler]},
-    )
-    return {"trimmed_transcription": response["output_text"].replace("```\n", "")}
+    response = model.generate_content(prompt.format(transcription=transcription_text))
+    return {"trimmed_transcription": response.text}
